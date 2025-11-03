24b1835714e24aa6f0f5ab1354ca0c82ffc13642
corvad <71835941+corvad@users.noreply.github.com>
2025-11-03 01:40:13 -0500

    completed unique-ids

---
+++ maelstrom-unique-ids/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+go build -v
+../vendor/maelstrom/maelstrom test -w unique-ids --bin maelstrom-unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
